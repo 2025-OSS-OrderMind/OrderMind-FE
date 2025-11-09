754247305ea22361acc46995e8f9627a6557aada
jdh3887 <jdh3887@gmail.com>
2025-11-09 15:07:21 +0900

    배경 색 통일 #6

--- src/pages/Analyzing/index.tsx
+++ src/pages/Analyzing/index.tsx
@@ -1,7 +1,7 @@
 function AnalyzingPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h2 className="text-3xl font-semibold">채팅 분석중...</h2>
+    <div className="flex justify-center items-center h-screen bg-[#F5FDE7]">
+      <h2 className="text-3xl font-semibold text-[#252525]">채팅 분석중...</h2>
     </div>
   );
 }
